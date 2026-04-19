15434a8af0d860a28dd0effd833376ecfa725528
chanchhay <chanchhay07@gmail.com>
2026-04-19 16:37:27 +0700

    fixed-social-login

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,20 @@ const nextConfig: NextConfig = {
         return [
             {
                 source: "/api/:path*",
-                destination: "https://expense-tracker-spring-boot-api-production.up.railway.app/api/:path*",
+                destination:
+                    "https://expense-tracker-spring-boot-api-production.up.railway.app/api/:path*",
+            },
+            // 1. Proxy the authorization route
+            {
+                source: "/oauth2/:path*",
+                destination:
+                    "https://expense-tracker-spring-boot-api-production.up.railway.app/oauth2/:path*",
+            },
+            // 2. Proxy the callback route
+            {
+                source: "/login/oauth2/:path*",
+                destination:
+                    "https://expense-tracker-spring-boot-api-production.up.railway.app/login/oauth2/:path*",
             },
         ];
     },
